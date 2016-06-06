6fa8cda9050bd4f6d86b1da22b63b812c2b37015
Naoki Hikita <hikitanaoki.dev@gmail.com>
2016-06-06 23:01:15 +0900

    test

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,4 +1,9 @@
 export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
+export LIBRARY_PATH="$LIBRARY_PATH:/app/mono/lib"
 export INCLUDE_PATH="$INCLUDE_PATH:/app/mono/include"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/app/mono/lib/pkgconfig"
+export MONO_CFG_DIR="/app/mono/etc"
+export MONO_CONFIG="/app/mono/etc/mono/config"
+export MONO_GAC_PREFIX="/app/mono"
 ln -s vendor/mono /app
